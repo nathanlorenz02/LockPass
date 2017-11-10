363688e0838467a739f8e36642a5c78f99b5b12f
Nathan Lorenz <nathanlorenz0417@gmail.com>
2017-11-10 12:06:03 -0800

    Initial Commit

---
+++ LockPass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LockPass
+//
+//  Created by Nathan Lorenz on 2017-11-10.
+//  Copyright © 2017 Nathan Lorenz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
